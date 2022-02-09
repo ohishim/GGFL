5b4c75a8bb862866d2aa0f4499ba235cf217767d
ohishim <m.ohishi.stat@gmail.com>
2022-02-09 15:46:21 +0900

    Update

--- R/exdata.R
+++ R/exdata.R
@@ -1,6 +1,3 @@
 
-# exdata <- read.table("data/exdata.txt", header = T)
-# adj <- read.table("data/adj.txt", header = T)
-
-# save(list=ls(), file="data/exdata.Rdata")
-load("data/exdata.Rdata")
+exdata <- read.table("data/exdata.txt", header = T)
+adj <- read.table("data/adj.txt", header = T)
